9092d383af861323d5d9ccf5390bc10c6e440f8a
Dmytro Tkachuk <dmytrooo3.tk@gmail.com>
2024-04-21 19:56:02 +0300

    Initial commit

---
+++ src/main/java/com/example/carpetcleanbot/CarpetCleanBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.carpetcleanbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpetCleanBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarpetCleanBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/carpetcleanbot/CarpetCleanBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carpetcleanbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpetCleanBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
